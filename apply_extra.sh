4d2cecd76219a2f148025c72551ef4f9ae7240e5
Ryan Gonzalez <rymg19@gmail.com>
2019-07-02 20:37:18 -0500

    Initial attempts at syncing with upstream

---
+++ apply_extra.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+tar xf firefox-*.tar.bz2
+rm firefox-*.tar.bz2
+
+ln -s /app/firefox/plugins/plugins firefox/browser
+mkdir -p firefox/{distribution/extensions,browser/defaults/preferences}
+ln -s /app/firefox/data/distribution.ini firefox/distribution
+ln -s /app/firefox/data/endless-default-prefs.js firefox/browser/defaults/preferences
+
+for xpi in *.xpi; do
+  mv $xpi firefox/distribution/extensions/langpack-${xpi%.*}@firefox.mozilla.org.xpi
+done
--- endless-default-prefs.js
+++ endless-default-prefs.js
@@ -3,3 +3,5 @@ pref("intl.locale.matchOS", true);
 pref("app.update.auto", false);
 pref("app.update.enabled", false);
 pref("app.update.silent", false);
+pref("app.update.autoInstallEnabled", false);
+pref("browser.shell.checkDefaultBrowser", false);
