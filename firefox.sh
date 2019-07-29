78da7555a371714a1aeb2c707e1e3933269ab706
Ryan Gonzalez <rymg19@gmail.com>
2019-07-29 15:34:29 -0500

    Revamp manifest in sync with upstream
    Upstream URL: https://hg.mozilla.org/projects/maple/file/tip/taskcluster/docker/firefox-flatpak/
    
    Commit: 278f506dd3abcabc076e8b9da1ec6756b8a6b8bd

--- firefox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Launch the script that installs the plugins (if needed) in the background
-# so it installs them while Firefox is already running, avoiding the user
-# to wait
-/app/bin/firefox-plugins-installer &
-exec /app/extra/firefox/firefox-bin $@
